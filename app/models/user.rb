2b95e16995c22de1e1aa1518a8c0e2b3bedeb772
paseri10mayo <83695208+paseri10mayo@users.noreply.github.com>
2021-06-04 12:03:23 +0900

    モデルの単体テストコード完成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
-  validates :birthday, presence: true
-end
+    with_options presence: true do
+     validates :nickname, :birthday, :password_confirmation
+     validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "は全角ひらがな、全角カタカナ、漢字で入力して下さい" }
+     validates :last_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい" }
+   end
+  validates :password, :password_confirmation,
+  format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "は半角英数で入力して下さい" }
+ end
