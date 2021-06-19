321ae8274531d7ea3969616f319a763416af304d
paseri10mayo <83695208+paseri10mayo@users.noreply.github.com>
2021-06-20 00:05:23 +0900

    コメント機能完成

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,6 +9,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :order
   has_one_attached :image
+  has_many :comments
   
   validates :price, numericality: { with: /\A[0-9]+\z/, message: 'Half-width number' }
   
